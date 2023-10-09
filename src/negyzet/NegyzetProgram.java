395f6050bb17e30489a3c183d8a76bca1c95ced6
Venc03 <vegi.dani1@gmail.com>
2023-10-09 13:14:27 +0200

    merge

--- src/negyzet/NegyzetProgram.java
+++ src/negyzet/NegyzetProgram.java
@@ -1,7 +1,6 @@
 package negyzet;
 
 public class NegyzetProgram {
-<<<<<<< HEAD
    public static Negyzet negyzet = new Negyzet();
 
     
@@ -10,15 +9,7 @@ public class NegyzetProgram {
         System.out.println("Négyzet x kooridnátája:" + negyzet.getX() + "\nNégyzet y koordinátája:" + negyzet.getY() + "\nterülete: " + (negyzet.terulet()));
 
        
-        
-        
-=======
-    private static int x;
-    private static int y;
-    
-    public static void main(String[] args) {
-        System.out.println("Negyzet x koordinataja: " + x + ", y koordinataja: " + y);
->>>>>>> 18baecbc9a3ea200da497b6d7c94a3ff9abece55
+       
     }
     
 }
