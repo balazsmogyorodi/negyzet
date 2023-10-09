402af241de236b662893d84b51e24e557ba8b0a8
Venc03 <vegi.dani1@gmail.com>
2023-10-09 12:49:00 +0200

    1 feladat

--- src/negyzet/NegyzetProgram.java
+++ src/negyzet/NegyzetProgram.java
@@ -1,9 +1,11 @@
 package negyzet;
 
 public class NegyzetProgram {
-
+    private static int x;
+    private static int y;
+    
     public static void main(String[] args) {
-        
+        System.out.println("Negyzet x koordinataja: " + x + ", y koordinataja: " + y);
     }
     
 }
